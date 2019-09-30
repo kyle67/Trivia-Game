e4c71b636d4ddb63189e2588f06cb0d30900b7dc
Kyle Daniels <kyledaniels@Kyles-MacBook-Air.local>
2019-09-30 17:30:22 -0400

    Modified index.html with container adding start buttom. Modified index.html with id=timer, id=game. Modified questions.js constructing array movieQuesions with object containing movie questions, answer choices, and answer choice.

--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,1 +1,27 @@
-console.log('testquestion');
+//console.log('testquestion');
+//create array movieQuestions with object 
+const movieQuestions = [
+    {
+        question:"In the 1951 movie The Day The Earth Stood Still which US city did the alien spaceship land in?",
+        choices: ["Washington DC","New York City","Los Angeles","Chicago"],
+        correctAnswer:"Washington DC"
+    },
+
+    {
+        question:"What performer from Cannonball Run fought off a murdering, lovesick robot in Saturn 3?",
+        choices: ["Babara Striestrand","Carol Burnett","Farrah Fawcett","Elizabeth Taylor"],
+        correctAnswer:"Farrah Fawcett" 
+    },
+ 
+    {
+        question:"",
+        choices: ["","","",""],
+        correctAnswer:""
+    },
+
+    {    
+        question:"",
+        choices: ["","","",""],
+        correctAnswer:""
+    },
+];
