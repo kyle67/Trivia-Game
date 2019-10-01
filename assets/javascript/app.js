ad81a14d2dff2778f318308bbc3eeac06086216d
Kyle Daniels <kyledaniels@Kyles-MacBook-Air.local>
2019-10-01 14:37:38 -0400

    Modified app.js define variables for askQuestions function.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,21 @@
 //console.log('testapp');
-//console.log($);verified juery talks to index.html
+//console.log($);verified juery linked to index.html
 //Create variables with initial variables
 var counter = 30;
-var currentmovieQuestion =0;
+var currentQuestion =0;
 var score = 0;
-var reset = 0;
+var lost = 0;
 var timer;
 
 //Create function aksQuestion to
 //display the question and the answer choices.
 
-function askQuestion() {
-    const question = movieQuestions[currentmovieQuestion].movieQuestions;//
-    const choices = movieQuestions [currentmovieQuestion].choices;//
-//Access id#game
+function askQuestion() 
+{
+    const question = movieQuestions[currentQuestion].question;
+    const choices = movieQuestions [currentQuestion].choices;
+    
+    $('#time').html('Timer:' + counter);
     $('#game').html('<h4>'+ question + '</h4>');
 
 }
